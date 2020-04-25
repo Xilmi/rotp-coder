07e677322fe743c7d3b41cda3d18555a348bc92c
coder111 <coder111@protonmail.com>
2020-04-25 23:35:30 +0100

    Rebase to 1.8c Beta

--- src/rotp/RotpGovernor.java
+++ src/rotp/RotpGovernor.java
@@ -24,8 +24,8 @@ import javax.swing.*;
  * to get more memory it uses the right jar file name.
  */
 public class RotpGovernor {
-    static String governorVersion = "1.8.1";
-    static String expectedROTPVersion = "Beta 1.8a";
+    static String governorVersion = "1.8.2";
+    static String expectedROTPVersion = "Beta 1.8c";
 
     public static void main(String[] args) {
         String jarFilename = "ROTP-" + governorVersion + "-governor.jar";
