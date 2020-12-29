f94c65e9e79f5f6dec913dc2cd1b418d814efa28
coder111 <coder111@protonmail.com>
2020-12-29 15:32:49 +0000

    2.04.3 Embed the format conversion plugin. Fix the launch and versions.

--- src/rotp/RotpGovernor.java
+++ src/rotp/RotpGovernor.java
@@ -16,34 +16,69 @@
 package rotp;
 
 import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Check for Remnants.jar version before launching.
- *
+ * <p>
  * Remnants.jar is now added to classpath automatically via manifest. So just make sure when ROTP relaunches
  * to get more memory it uses the right jar file name.
  */
 public class RotpGovernor {
-    // minified versions will use WebP images and Ogg sounds.
-    public static boolean minified = false;
-
-    static String governorVersion = "2.04.1";
     static String expectedROTPVersion = "Beta 2.04";
 
-    public static void main(String[] args) {
-        String jarFilename = "ROTP-" + governorVersion + "-governor.jar";
+    private static String governorVersion = null;
+
+    public static String governorVersion() {
+        if (governorVersion == null) {
+            try {
+                governorVersion = readVersion();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+        return governorVersion;
+    }
+
+    // minified versions will use WebP images and Ogg sounds.
+    private static Boolean mini = null;
+
+    public static String miniSuffix() {
+        if (mini == null) {
+            try {
+                mini = readMini();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+        if (mini) {
+            return "-mini";
+        } else {
+            return "";
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        String jarFilename = "ROTP-" + governorVersion() + "-governor.jar";
         try {
             Class.forName("rotp.Rotp");
         } catch (ClassNotFoundException e) {
-            String message = "Unable to find Remnants.jar\n"+
-                    "Place "+jarFilename+" in the same directory as Remnants.jar and try again";
+            String message = "Unable to find Remnants.jar\n" +
+                    "Place " + jarFilename + " in the same directory as Remnants.jar and try again";
 
             System.out.println(message);
             JOptionPane.showMessageDialog(null, message, "Remnants.jar not found", JOptionPane.ERROR_MESSAGE);
             System.exit(2);
         }
         if (!expectedROTPVersion.equals(Rotp.releaseId)) {
-            System.out.println("Version mismatch. Governor " + governorVersion +
+            System.out.println("Version mismatch. Governor " + governorVersion() +
                     " expects ROTP " + expectedROTPVersion +
                     " but actual is " + Rotp.releaseId);
             Object result = JOptionPane.showInputDialog(null,
@@ -61,5 +96,27 @@ public class RotpGovernor {
         }
         Rotp.jarFileName = jarFilename;
         Rotp.main(args);
+    }
+
+    private static String readVersion() throws IOException {
+        try (InputStream is = RotpGovernor.class.getResourceAsStream("/rotp-version.properties")) {
+            if (is == null) {
+                throw new FileNotFoundException("Unable to find rotp-version.properties");
+            }
+            Properties properties = new Properties();
+            properties.load(is);
+            return properties.getProperty("version");
+        }
+    }
+
+    private static boolean readMini() throws IOException {
+        try (InputStream is = RotpGovernor.class.getResourceAsStream("/rotp-mini.properties")) {
+            if (is == null) {
+                return false;
+            }
+            Properties properties = new Properties();
+            properties.load(is);
+            return "true".equalsIgnoreCase(properties.getProperty("mini"));
+        }
     }
 }
