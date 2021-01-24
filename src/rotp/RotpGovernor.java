e02407f38f3319b4fec2855af7b4514478261dec
modnar-hajile <66027258+modnar-hajile@users.noreply.github.com>
2021-01-24 11:33:31 -0600

    update to coder governor 2.10.1

--- src/rotp/RotpGovernor.java
+++ src/rotp/RotpGovernor.java
@@ -28,7 +28,7 @@ import java.util.Properties;
  * to get more memory it uses the right jar file name.
  */
 public class RotpGovernor {
-    static String expectedROTPVersion = "Beta 2.09b";
+    static String expectedROTPVersion = "Beta 2.10";
 
     private static String governorVersion = null;
 
