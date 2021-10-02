a9cc5a0e874bd1f55fa426bdbc1864ada2ba877d
modnar-hajile <66027258+modnar-hajile@users.noreply.github.com>
2021-10-02 06:04:35 -0500

    MOD35 release

--- src/rotp/RotpGovernor.java
+++ src/rotp/RotpGovernor.java
@@ -28,7 +28,7 @@ import java.util.Properties;
  * to get more memory it uses the right jar file name.
  */
 public class RotpGovernor {
-    static String expectedROTPVersion = "0.93";
+    static String expectedROTPVersion = "0.94";
 
     private static String governorVersion = null;
 
