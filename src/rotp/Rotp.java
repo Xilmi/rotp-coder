4e33c9ee73a33285de8ed6d4cdc9fd6eb8e4027e
Xilmi <ail.st@gmx.de>
2022-01-10 00:33:45 +0100

    New version prepared

--- src/rotp/Rotp.java
+++ src/rotp/Rotp.java
@@ -51,7 +51,7 @@ public class Rotp {
     public static boolean countWords = false;
     private static String startupDir;
     private static JFrame frame;
-    public static String releaseId = "Fusion-Mod 1.02.1";
+    public static String releaseId = "Fusion-Mod 1.02.2";
     public static long startMs = System.currentTimeMillis();
     public static long maxHeapMemory = Runtime.getRuntime().maxMemory() / 1048576;
     public static long maxUsedMemory;
